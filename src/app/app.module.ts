d6b27aee7ec5821d33df7cb682bf4e6b6c59e24e
Frejus DASSI <frejohope@gmail.com>
2024-12-17 11:10:22 +0100

    création des components de tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './components/tasks/task/task.component';
+import { TaskGroupComponent } from './components/tasks/task-group/task-group.component';
+import { TaskGroupListComponent } from './components/tasks/task-group-list/task-group-list.component';
+import { SearchTaskComponent } from './components/tasks/search-task/search-task.component';
+import { FormAddTaskComponent } from './components/tasks/form-add-task/form-add-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    TaskGroupComponent,
+    TaskGroupListComponent,
+    SearchTaskComponent,
+    FormAddTaskComponent
   ],
   imports: [
     BrowserModule,
